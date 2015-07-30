a62df9256fafc3ea8dc1156f95fbe61d5f8079c9
fredo121 <fredo121@toothwit.ch>
2015-07-30 13:58:12 +0200

    Fixed eclipse classpath ( no tests run ) and removed unused dependencies

--- src/main/java/ch/toothwit/antirain/events/ServerEventListener.java
+++ src/main/java/ch/toothwit/antirain/events/ServerEventListener.java
@@ -1,11 +1,9 @@
 package ch.toothwit.antirain.events;
 
 
-import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.weather.WeatherChangeEvent;
-import ch.toothwit.antirain.main.AntiRain;
+import org.bukkit.event.weather.WeatherChangeEvent; 
 
 public class ServerEventListener implements Listener {
 	@EventHandler
